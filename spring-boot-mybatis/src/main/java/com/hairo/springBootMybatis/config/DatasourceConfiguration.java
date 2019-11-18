d1700937442a8d5d55d4fadf5d54a03f5c76e733
Hairo <1871157779@qq.com>
2019-11-18 17:37:13 +0800

    实现Spring-boot-mybatis整合c3p0连接池

---
+++ spring-boot-mybatis/src/main/java/com/hairo/springBootMybatis/config/DatasourceConfiguration.java
@@ -0,0 +1,25 @@
+package com.hairo.springBootMybatis.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @author ： Hairo
+ * @date : 2019/11/18 17:32
+ */
+@Configuration
+public class DatasourceConfiguration {
+    @Bean(name = "dataSource")
+    @Qualifier(value = "dataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "c3p0")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().type(com.mchange.v2.c3p0.ComboPooledDataSource.class).build();
+    }
+}
